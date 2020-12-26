23ffeac713e196dbbcc76547ab014e3a782876f4
Discussion4all <discussion4all@yahoo.com>
2020-12-26 14:43:19 +0530

    optimize code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import routes from "./routes/routes";
 import {connect,Provider} from 'react-redux'
-import './app.css';
+import './App.css';
 import "./style/css/style.css";
 import {setToken,setName} from './actions/userAction'
 import {setInitialState,setAllQuestion,setOneQuestion,setquizType,setAllAnswers} from './actions/questionAction'
